a4bcd7caaa226bfa87d05b3016268097a02ec4c3
Hiitler <mehmoodkanfast@gmail.com>
2025-11-27 10:59:23 +0500

    Updated rand.cpp

--- rand.cpp
+++ rand.cpp
@@ -5,14 +5,9 @@
 #include <cstdlib>
 #include <ctime>
 int main() {
-    // Seed the random number generator
     std::srand(static_cast<unsigned int>(std::time(0)));
-
-    // Generate a random number between 1 and 100
     int randomNumber = std::rand() % 100 + 1;
-
-    // Output the random number
-    std::cout << "Random number between 1 and 100: " << randomNumber << std::endl;
+  std::cout << "Random number between 1 and 100: " << randomNumber << std::endl;
 
     return 0;
 }
